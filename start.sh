e829f2dc32bd58e2f61dcc96334858d07cd8a6b1
dpfeiffer <d.pfeiffer404@gmail.com>
2017-09-13 20:41:11 +0200

    add native memory tracking

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 exec java \
+    -XX:NativeMemoryTracking=summary \
+    -XshowSettings:vm \
     -Dplay.http.secret.key=${PLAY_HTTP_SECRET_KEY} \
     -jar /app/application.jar
